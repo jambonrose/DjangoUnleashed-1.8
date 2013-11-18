98a8ef0d11c81006d12e4c01cccd044bccb7fdb8
Andrew Pinkham <code@andrewsforge.com>
2013-11-18 13:43:07 -0600

    Ch02: Code Hello World web page route.
    A website is two things: content, and a route to that content. A URL
    pattern in Django defines the route.

--- suorganizer/urls.py
+++ suorganizer/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from helloworld.views import greeting
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', greeting),
 ]
