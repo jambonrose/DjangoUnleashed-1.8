dfb719fa8cd092697fe02fbab59c4395c36385fa
Andrew Pinkham <code@andrewsforge.com>
2013-11-18 18:09:09 -0600

    Ch03: Declare Post model. [skip ci]
    A model tells Django how to handle data in Python and in our database of
    choice.
    
        https://docs.djangoproject.com/en/1.8/topics/db/models/
    
    The (blog) Post model will contain all of the information for a single
    dated web article.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+# Model Field Reference
+# https://docs.djangoproject.com/en/1.8/ref/models/fields/
+
+
+class Post(models.Model):
+    pass
