d3baa05f7dc5143883ddd01e8bf4f13071638fdb
Andrew Pinkham <code@andrewsforge.com>
2013-11-20 14:03:12 -0600

    Ch03: Code str method for Post model. [skip ci]

--- blog/models.py
+++ blog/models.py
@@ -21,3 +21,8 @@ class Post(models.Model):
         Tag, related_name='blog_posts')
     startups = models.ManyToManyField(
         Startup, related_name='blog_posts')
+
+    def __str__(self):
+        return "{} on {}".format(
+            self.title,
+            self.pub_date.strftime('%Y-%m-%d'))
