e9df4858631d9efdcb6a5b960c25f64cae875661
Andrew Pinkham <code@andrewsforge.com>
2013-11-20 12:54:02 -0600

    Ch03: Add options to Post model fields. [skip ci]

--- blog/models.py
+++ blog/models.py
@@ -9,8 +9,15 @@ from organizer.models import Startup, Tag
 
 class Post(models.Model):
     title = models.CharField(max_length=63)
-    slug = models.SlugField()
+    slug = models.SlugField(
+        max_length=63,
+        help_text='A label for URL config',
+        unique_for_month='pub_date')
     text = models.TextField()
-    pub_date = models.DateField()
-    tags = models.ManyToManyField(Tag)
-    startups = models.ManyToManyField(Startup)
+    pub_date = models.DateField(
+        'date published',
+        auto_now_add=True)
+    tags = models.ManyToManyField(
+        Tag, related_name='blog_posts')
+    startups = models.ManyToManyField(
+        Startup, related_name='blog_posts')
