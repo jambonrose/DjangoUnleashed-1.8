e2e1ea416d38565a419fff75f6ad4b776b74bc8e
Andrew Pinkham <code@andrewsforge.com>
2013-11-20 14:33:40 -0600

    Ch03: Declare Meta class in Post model. [skip ci]

--- blog/models.py
+++ blog/models.py
@@ -22,6 +22,11 @@ class Post(models.Model):
     startups = models.ManyToManyField(
         Startup, related_name='blog_posts')
 
+    class Meta:
+        verbose_name = 'blog post'
+        ordering = ['-pub_date', 'title']
+        get_latest_by = 'pub_date'
+
     def __str__(self):
         return "{} on {}".format(
             self.title,
