342fdb6a6b0b56bdabf61a52f0c28852eda7f61f
Andrew Pinkham <code@andrewsforge.com>
2013-11-20 12:38:31 -0600

    Ch03: Declare Tag, Startup, NewsLink models. [skip ci]
    A model tells Django how to handle data in Python and in our database of
    choice.
    
        https://docs.djangoproject.com/en/1.8/topics/db/models/
    
    The Tag model is the site's metadata, providing a loose system to
    categorize and organize other data.
    
    The Startup model contains information about local startup businesses.
    
    The NewsLink model links to news articles about startups on other
    websites.

--- organizer/models.py
+++ organizer/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+# Model Field Reference
+# https://docs.djangoproject.com/en/1.8/ref/models/fields/
+
+
+class Tag(models.Model):
+    pass
+
+
+class Startup(models.Model):
+    pass
+
+
+class NewsLink(models.Model):
+    pass
