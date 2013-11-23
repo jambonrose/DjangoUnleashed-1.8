ef2c075c3fd1debd4edbce044d35964cdb7cd81e
Andrew Pinkham <code@andrewsforge.com>
2013-11-22 19:31:22 -0600

    Ch04: Code Hello World web page content (view).

--- organizer/views.py
+++ organizer/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
 
-# Create your views here.
+
+def homepage(request):
+    return HttpResponse('Hello (again) World!')
