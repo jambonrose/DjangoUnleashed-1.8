c4a0b6a5b40f7ed964ef43533c4a761124b2ea06
Andrew Pinkham <code@andrewsforge.com>
2013-11-24 15:55:48 -0600

    Ch04: Modify homepage view to list Tags.

--- organizer/views.py
+++ organizer/views.py
@@ -1,5 +1,9 @@
 from django.http.response import HttpResponse
 
+from .models import Tag
+
 
 def homepage(request):
-    return HttpResponse('Hello (again) World!')
+    tag_list = Tag.objects.all()
+    output = ", ".join([tag.name for tag in tag_list])
+    return HttpResponse(output)
