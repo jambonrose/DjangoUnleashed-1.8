95b20c151bf1f60eaab12e3f161e2b49b7026f69
Andrew Pinkham <code@andrewsforge.com>
2013-11-24 15:50:03 -0600

    Ch04: Code Hello World web page route (URL conf).

--- suorganizer/urls.py
+++ suorganizer/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from organizer.views import homepage
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', homepage),
 ]
