614a996dd8227808e796a369ed0faf1f9427f780
Andrew Pinkham <code@andrewsforge.com>
2013-12-21 17:52:44 +0800

    Ch04: Use template in homepage view.

--- organizer/views.py
+++ organizer/views.py
@@ -1,22 +1,13 @@
 from django.http.response import HttpResponse
+from django.template import Context, loader
 
 from .models import Tag
 
 
 def homepage(request):
     tag_list = Tag.objects.all()
-    html_output = "<html>\n"
-    html_output += "<head>\n"
-    html_output += "  <title>"
-    html_output += "Don't Do This!</title>\n"
-    html_output += "</head>\n"
-    html_output += "<body>\n"
-    html_output += "  <ul>\n"
-    for tag in tag_list:
-        html_output += "    <li>"
-        html_output += tag.name.title()
-        html_output += "</li>\n"
-    html_output += "  </ul>\n"
-    html_output += "</body>\n"
-    html_output += "</html>\n"
-    return HttpResponse(html_output)
+    template = loader.get_template(
+        'organizer/tag_list.html')
+    context = Context({'tag_list': tag_list})
+    output = template.render(context)
+    return HttpResponse(output)
