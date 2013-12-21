5b18131069f860b712d8e54611541a8729496867
Andrew Pinkham <code@andrewsforge.com>
2013-12-21 18:17:21 +0800

    Ch05: Create URL pattern for Tag Detail.

--- suorganizer/urls.py
+++ suorganizer/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from organizer.views import homepage
+from organizer.views import homepage, tag_detail
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', homepage),
+    url(r'^tag/(?P<slug>[\w\-]+)/$',
+        tag_detail,
+        ),
 ]
