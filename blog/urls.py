cb5dd59383483fa2bd0238eb0dd9581e8ebe9adf
Andrew Pinkham <code@andrewsforge.com>
2013-12-23 16:52:20 +0800

    Ch05: Create and route Post Detail.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url
 
-from .views import post_list
+from .views import post_detail, post_list
 
 urlpatterns = [
     url(r'^$',
         post_list,
         name='blog_post_list'),
+    url(r'^(?P<year>\d{4})/'
+        r'(?P<month>\d{1,2})/'
+        r'(?P<slug>[\w\-]+)/$',
+        post_detail,
+        name='blog_post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    get_object_or_404, render)
 
 from .models import Post
+
+
+def post_detail(request, year, month, slug):
+    post = get_object_or_404(
+        Post,
+        pub_date__year=year,
+        pub_date__month=month,
+        slug=slug)
+    return render(
+        request,
+        'blog/post_detail.html',
+        {'post': post})
 
 
 def post_list(request):
