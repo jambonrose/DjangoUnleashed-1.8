5014777e7e6a472367ab376335353accf0c91056
Andrew Pinkham <code@andrewsforge.com>
2014-01-09 17:46:41 -0600

    Ch07: Implement TagForm save method.

--- organizer/forms.py
+++ organizer/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 
+from .models import Tag
+
 
 class TagForm(forms.Form):
     name = forms.CharField(max_length=31)
     slug = forms.SlugField(
         max_length=31,
         help_text='A label for URL config')
+
+    def save(self):
+        new_tag = Tag.objects.create(
+            name=self.cleaned_data['name'],
+            slug=self.cleaned_data['slug'])
+        return new_tag
