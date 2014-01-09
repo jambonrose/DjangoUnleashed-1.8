5fb0dff63a1ff9bbc3c9c5730ec60c56d7cc871e
Andrew Pinkham <code@andrewsforge.com>
2014-01-09 10:37:54 -0600

    Ch05: Redirect root with path shortcut.

--- suorganizer/views.py
+++ suorganizer/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
 
 
 def redirect_root(request):
-    return HttpResponseRedirect('/blog/')
+    return redirect('/blog/')
