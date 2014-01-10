cc75d1162f1f1c4d658ff6d74eb7004ee3fd5645
Andrew Pinkham <code@andrewsforge.com>
2014-01-09 18:00:18 -0600

    Ch09: Route Tag Create view with URL pattern.

--- organizer/urls.py
+++ organizer/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 
 from .views import (
-    startup_detail, startup_list, tag_detail,
-    tag_list)
+    startup_detail, startup_list, tag_create,
+    tag_detail, tag_list)
 
 urlpatterns = [
     url(r'^startup/$',
@@ -14,6 +14,9 @@ urlpatterns = [
     url(r'^tag/$',
         tag_list,
         name='organizer_tag_list'),
+    url(r'^tag/create/$',
+        tag_create,
+        name='organizer_tag_create'),
     url(r'^tag/(?P<slug>[\w\-]+)/$',
         tag_detail,
         name='organizer_tag_detail'),
