30726ed0ed50af7deb50ddce569968d937f96fa3
Andrew Pinkham <code@andrewsforge.com>
2014-01-11 19:06:54 -0600

    Ch07: Implement TagForm clean_slug method.

--- organizer/forms.py
+++ organizer/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import Tag
 
@@ -11,6 +12,14 @@ class TagForm(forms.Form):
 
     def clean_name(self):
         return self.cleaned_data['name'].lower()
+
+    def clean_slug(self):
+        new_slug = (
+            self.cleaned_data['slug'].lower())
+        if new_slug == 'create':
+            raise ValidationError(
+                'Slug may not be "create".')
+        return new_slug
 
     def save(self):
         new_tag = Tag.objects.create(
