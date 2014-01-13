d61714022eb191294373519e41d6c1ec3252ed39
Andrew Pinkham <code@andrewsforge.com>
2014-01-13 10:13:02 -0600

    Ch07: Refactor TagForm to inherit ModelForm.

--- organizer/forms.py
+++ organizer/forms.py
@@ -4,11 +4,10 @@ from django.core.exceptions import ValidationError
 from .models import Tag
 
 
-class TagForm(forms.Form):
-    name = forms.CharField(max_length=31)
-    slug = forms.SlugField(
-        max_length=31,
-        help_text='A label for URL config')
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = '__all__'
 
     def clean_name(self):
         return self.cleaned_data['name'].lower()
@@ -20,9 +19,3 @@ class TagForm(forms.Form):
             raise ValidationError(
                 'Slug may not be "create".')
         return new_slug
-
-    def save(self):
-        new_tag = Tag.objects.create(
-            name=self.cleaned_data['name'],
-            slug=self.cleaned_data['slug'])
-        return new_tag
