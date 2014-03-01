928c982c0378b41d2600e8c9d34ef584879f7117
Andrew Pinkham <code@andrewsforge.com>
2014-02-28 18:05:07 -0600

    Ch05: Create and route Post List.

--- blog/urls.py
+++ blog/urls.py
@@ -1,2 +1,9 @@
+from django.conf.urls import url
+
+from .views import post_list
+
 urlpatterns = [
+    url(r'^$',
+        post_list,
+        name='blog_post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    return render(
+        request,
+        'blog/post_list.html',
+        {'post_list': Post.objects.all()})
