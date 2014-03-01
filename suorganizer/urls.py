02dabec0934ed3d3e7d7b65dc1d80717ee7d51b3
Andrew Pinkham <code@andrewsforge.com>
2014-02-28 18:02:22 -0600

    Ch05: Create URL Config for blog app.

--- suorganizer/urls.py
+++ suorganizer/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from blog import urls as blog_urls
 from organizer import urls as organizer_urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include(blog_urls)),
     url(r'^', include(organizer_urls)),
 ]
