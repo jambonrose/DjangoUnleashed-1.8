d2ecb7f70d6ad3803b3fba0f375ec32d51a65621
Andrew Pinkham <code@andrewsforge.com>
2014-04-02 18:54:58 -0500

    Ch05: Shorten views with render.

--- organizer/views.py
+++ organizer/views.py
@@ -1,27 +1,20 @@
-from django.http.response import HttpResponse
-from django.shortcuts import get_object_or_404
-from django.template import RequestContext, loader
+from django.shortcuts import (
+    get_object_or_404, render)
 
 from .models import Tag
 
 
 def homepage(request):
-    tag_list = Tag.objects.all()
-    template = loader.get_template(
-        'organizer/tag_list.html')
-    context = RequestContext(
+    return render(
         request,
-        {'tag_list': tag_list})
-    output = template.render(context)
-    return HttpResponse(output)
+        'organizer/tag_list.html',
+        {'tag_list': Tag.objects.all()})
 
 
 def tag_detail(request, slug):
     tag = get_object_or_404(
         Tag, slug__iexact=slug)
-    template = loader.get_template(
-        'organizer/tag_detail.html')
-    context = RequestContext(
+    return render(
         request,
+        'organizer/tag_detail.html',
         {'tag': tag})
-    return HttpResponse(template.render(context))
