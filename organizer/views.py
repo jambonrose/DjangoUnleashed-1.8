4d36d603db90a53d5ca2cfc51478c0091997a86c
Andrew Pinkham <code@andrewsforge.com>
2014-04-04 21:15:40 -0500

    Ch05: Remove render_to_request shortcut (revert).

--- organizer/views.py
+++ organizer/views.py
@@ -1,18 +1,23 @@
-from django.shortcuts import (
-    get_object_or_404, render_to_response)
+from django.http.response import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.template import Context, loader
 
 from .models import Tag
 
 
 def homepage(request):
-    return render_to_response(
-        'organizer/tag_list.html',
-        {'tag_list': Tag.objects.all()})
+    tag_list = Tag.objects.all()
+    template = loader.get_template(
+        'organizer/tag_list.html')
+    context = Context({'tag_list': tag_list})
+    output = template.render(context)
+    return HttpResponse(output)
 
 
 def tag_detail(request, slug):
     tag = get_object_or_404(
         Tag, slug__iexact=slug)
-    return render_to_response(
-        'organizer/tag_detail.html',
-        {'tag': tag})
+    template = loader.get_template(
+        'organizer/tag_detail.html')
+    context = Context({'tag': tag})
+    return HttpResponse(template.render(context))
