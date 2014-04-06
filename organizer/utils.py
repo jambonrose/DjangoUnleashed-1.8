453d1542642acac7cb3dc39b0001b380391638d8
Andrew Pinkham <code@andrewsforge.com>
2014-04-05 23:13:09 -0500

    Ch09: Implement ObjectCreateMixin.

---
+++ organizer/utils.py
@@ -0,0 +1,23 @@
+from django.shortcuts import redirect, render
+
+
+class ObjectCreateMixin:
+    form_class = None
+    template_name = ''
+
+    def get(self, request):
+        return render(
+            request,
+            self.template_name,
+            {'form': self.form_class()})
+
+    def post(self, request):
+        bound_form = self.form_class(request.POST)
+        if bound_form.is_valid():
+            new_object = bound_form.save()
+            return redirect(new_object)
+        else:
+            return render(
+                request,
+                self.template_name,
+                {'form': bound_form})
