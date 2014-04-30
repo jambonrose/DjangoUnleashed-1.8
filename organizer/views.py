5ff3dee4fa59710305b1b29b1c6a7811f32a81ab
Andrew Pinkham <code@andrewsforge.com>
2014-04-30 10:33:52 -0500

    Ch05: Shorten Tag Detail with render_to_response.

--- organizer/views.py
+++ organizer/views.py
@@ -1,23 +1,18 @@
-from django.http.response import HttpResponse
-from django.shortcuts import get_object_or_404
-from django.template import Context, loader
+from django.shortcuts import (
+    get_object_or_404, render_to_response)
 
 from .models import Tag
 
 
 def homepage(request):
-    tag_list = Tag.objects.all()
-    template = loader.get_template(
-        'organizer/tag_list.html')
-    context = Context({'tag_list': tag_list})
-    output = template.render(context)
-    return HttpResponse(output)
+    return render_to_response(
+        'organizer/tag_list.html',
+        {'tag_list': Tag.objects.all()})
 
 
 def tag_detail(request, slug):
     tag = get_object_or_404(
         Tag, slug__iexact=slug)
-    template = loader.get_template(
-        'organizer/tag_detail.html')
-    context = Context({'tag': tag})
-    return HttpResponse(template.render(context))
+    return render_to_response(
+        'organizer/tag_detail.html',
+        {'tag': tag})
