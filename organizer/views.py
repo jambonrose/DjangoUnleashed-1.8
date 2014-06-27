dd5e4e1a713cc745a5c9d8a9a54338832023b747
Andrew Pinkham <code@andrewsforge.com>
2014-06-27 11:46:57 -0500

    Ch04: Manually list tags with HTML on homepage.

--- organizer/views.py
+++ organizer/views.py
@@ -5,5 +5,18 @@ from .models import Tag
 
 def homepage(request):
     tag_list = Tag.objects.all()
-    output = ", ".join([tag.name for tag in tag_list])
-    return HttpResponse(output)
+    html_output = "<html>\n"
+    html_output += "<head>\n"
+    html_output += "  <title>"
+    html_output += "Don't Do This!</title>\n"
+    html_output += "</head>\n"
+    html_output += "<body>\n"
+    html_output += "  <ul>\n"
+    for tag in tag_list:
+        html_output += "    <li>"
+        html_output += tag.name.title()
+        html_output += "</li>\n"
+    html_output += "  </ul>\n"
+    html_output += "</body>\n"
+    html_output += "</html>\n"
+    return HttpResponse(html_output)
