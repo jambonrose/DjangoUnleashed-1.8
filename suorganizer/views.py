720db559abac0365d5583779bb9e93acf969a7e4
Andrew Pinkham <code@andrewsforge.com>
2014-06-29 13:25:02 -0500

    Ch06: Revisit Redirect: use reverse.

--- suorganizer/views.py
+++ suorganizer/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 
 
 def redirect_root(request):
-    return redirect('blog_post_list')
+    url_path = reverse('blog_post_list')
+    return HttpResponseRedirect(url_path)
