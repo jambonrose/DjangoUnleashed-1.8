4a337182135f9eab580bcb2c7a6c7b0b3c0c2527
Andrew Pinkham <code@andrewsforge.com>
2014-07-01 13:28:20 -0500

    Ch07: Tag&Startup Forms inherit SlugCleanMixin.

--- organizer/forms.py
+++ organizer/forms.py
@@ -22,32 +22,18 @@ class SlugCleanMixin:
         return new_slug
 
 
-class StartupForm(forms.ModelForm):
+class StartupForm(
+        SlugCleanMixin, forms.ModelForm):
     class Meta:
         model = Startup
         fields = '__all__'
 
-    def clean_slug(self):
-        new_slug = (
-            self.cleaned_data['slug'].lower())
-        if new_slug == 'create':
-            raise ValidationError(
-                'Slug may not be "create".')
-        return new_slug
 
-
-class TagForm(forms.ModelForm):
+class TagForm(
+        SlugCleanMixin, forms.ModelForm):
     class Meta:
         model = Tag
         fields = '__all__'
 
     def clean_name(self):
         return self.cleaned_data['name'].lower()
-
-    def clean_slug(self):
-        new_slug = (
-            self.cleaned_data['slug'].lower())
-        if new_slug == 'create':
-            raise ValidationError(
-                'Slug may not be "create".')
-        return new_slug
