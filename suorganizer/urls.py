b02e39dac0e94bf60cc09ecb2c69391870f8f057
Andrew Pinkham <code@andrewsforge.com>
2015-01-12 21:39:04 -0600

    Ch17: Redirect site homepage with GCBV.

--- suorganizer/urls.py
+++ suorganizer/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.flatpages import \
     urls as flatpage_urls
+from django.views.generic import RedirectView
 
 from blog import urls as blog_urls
 from contact import urls as contact_urls
@@ -24,10 +25,11 @@ from organizer.urls import (
     newslink as newslink_urls,
     startup as startup_urls, tag as tag_urls)
 
-from .views import redirect_root
-
 urlpatterns = [
-    url(r'^$', redirect_root),
+    url(r'^$',
+        RedirectView.as_view(
+            pattern_name='blog_post_list',
+            permanent=False)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include(blog_urls)),
     url(r'^contact/', include(contact_urls)),
--- suorganizer/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import redirect
-
-
-def redirect_root(request):
-    return redirect('blog_post_list')
