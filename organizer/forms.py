0155c2719997631ebf05c933063481d1bc161356
Andrew Pinkham <code@andrewsforge.com>
2015-01-22 15:31:03 -0600

    Ch07: Demonstrate TagForm fields option list.

--- organizer/forms.py
+++ organizer/forms.py
@@ -7,7 +7,7 @@ from .models import Tag
 class TagForm(forms.ModelForm):
     class Meta:
         model = Tag
-        fields = '__all__'
+        fields = ['name', 'slug']
 
     def clean_name(self):
         return self.cleaned_data['name'].lower()
