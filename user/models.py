271c91a607797bbd9d1a1e179745aedf580e8209
Andrew Pinkham <code@andrewsforge.com>
2015-03-23 10:20:58 -0500

    Ch22: Add name and joined date field to Profile.

--- user/models.py
+++ user/models.py
@@ -8,10 +8,15 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL)
+    name = models.CharField(
+        max_length=255)
     slug = models.SlugField(
         max_length=30,
         unique=True)
     about = models.TextField()
+    joined = models.DateTimeField(
+        "Date Joined",
+        auto_now_add=True)
 
     def __str__(self):
         return self.user.get_username()
@@ -51,3 +56,9 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def get_absolute_url(self):
         return self.profile.get_absolute_url()
+
+    def get_full_name(self):
+        return self.profile.name
+
+    def get_short_name(self):
+        return self.profile.name
