477533df0e2decf68f72763c72d7811ea54a3be7
Andrew Pinkham <code@andrewsforge.com>
2015-03-25 11:57:56 -0500

    Ch28: Implement PostArchiveSitemap.

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -1,7 +1,10 @@
 from datetime import date
+from itertools import chain
 from math import log10
+from operator import itemgetter
 
 from django.contrib.sitemaps import Sitemap
+from django.core.urlresolvers import reverse
 
 from .models import Post
 
@@ -41,3 +44,47 @@ class PostSitemap(Sitemap):
             return normalized + 0.5
         else:
             return 0.5
+
+
+class PostArchiveSitemap(Sitemap):
+
+    def items(self):
+        year_dates = (
+            Post.objects.published().dates(
+                'pub_date', 'year', order='DESC',
+            ).iterator())
+        month_dates = (
+            Post.objects.published().dates(
+                'pub_date', 'month', order='DESC',
+            ).iterator())
+        year_tuples = map(
+            lambda d: (d, 'y'),
+            year_dates)
+        month_tuples = map(
+            lambda d: (d, 'm'),
+            month_dates)
+        return sorted(
+            chain(month_tuples, year_tuples),
+            key=itemgetter(0),
+            reverse=True)
+
+    def location(self, date_tuple):
+        archive_date, archive_type = date_tuple
+        if archive_type == 'y':
+            return reverse(
+                'blog_post_archive_year',
+                kwargs={
+                    'year': archive_date.year})
+        elif archive_type == 'm':
+            return reverse(
+                'blog_post_archive_month',
+                kwargs={
+                    'year': archive_date.year,
+                    'month': archive_date.month})
+        else:
+            raise NotImplementedError(
+                "{} did not recognize "
+                "{} denoted '{}'.".format(
+                    self.__class__.__name__,
+                    'archive_type',
+                    archive_type))
--- suorganizer/sitemaps.py
+++ suorganizer/sitemaps.py
@@ -1,8 +1,10 @@
-from blog.sitemaps import PostSitemap
+from blog.sitemaps import (
+    PostArchiveSitemap, PostSitemap)
 from organizer.sitemaps import (
     StartupSitemap, TagSitemap)
 
 sitemaps = {
+    'post-archives': PostArchiveSitemap,
     'posts': PostSitemap,
     'startups': StartupSitemap,
     'tags': TagSitemap,
