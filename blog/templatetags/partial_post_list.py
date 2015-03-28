2114ec3f93ba3f9a721d91c3d7d454761bba6992
Andrew Pinkham <code@andrewsforge.com>
2015-03-28 13:30:19 -0500

    Ch27: Add permission check in partial post tag.

--- blog/templatetags/partial_post_list.py
+++ blog/templatetags/partial_post_list.py
@@ -6,8 +6,16 @@ register = template.Library()
 
 
 @register.inclusion_tag(
-    'blog/includes/partial_post_list.html')
-def format_post_list(post_list):
+    'blog/includes/partial_post_list.html',
+    takes_context=True)
+def format_post_list(context, detail_object):
+    request = context.get('request')
+    future_perms = request.user.has_perm(
+        'blog.view_future_post')
+    if future_perms:
+        post_list = detail_object.blog_posts.all()
+    else:
+        post_list = detail_object.published_posts
     return {
         'post_list': post_list,
     }
