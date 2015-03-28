8ae4396137380c29e618b4619b93659de17aec7f
Andrew Pinkham <code@andrewsforge.com>
2015-03-28 13:07:26 -0500

    Ch27: Modify partial post list into inclusion tag.

--- blog/templatetags/partial_post_list.py
+++ blog/templatetags/partial_post_list.py
@@ -1,5 +1,3 @@
-from io import StringIO
-
 from django import template
 
 # https://docs.djangoproject.com/en/1.8/howto/custom-template-tags/
@@ -7,18 +5,9 @@ from django import template
 register = template.Library()
 
 
-@register.simple_tag
+@register.inclusion_tag(
+    'blog/includes/partial_post_list.html')
 def format_post_list(post_list):
-    indent = '  '
-    output = StringIO()
-    output.write('<ul>\n')
-    for post in post_list:
-        output.write(
-            '{}<li><a href="{}">\n'.format(
-                indent, post.get_absolute_url()))
-        output.write('{}{}\n'.format(
-            indent * 2, post.title.title()))
-        output.write('{}</a></li>\n'.format(
-            indent))
-    output.write('</ul>\n')
-    return output.getvalue()
+    return {
+        'post_list': post_list,
+    }
