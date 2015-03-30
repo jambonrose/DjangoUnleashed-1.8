e2738eb8b276893905f7954b2c05ca5b9a0353a5
Andrew Pinkham <code@andrewsforge.com>
2015-03-30 10:45:45 -0500

    Ch27: Add use of as keyword to get_latest_posts.

--- blog/templatetags/blog_latest.py
+++ blog/templatetags/blog_latest.py
@@ -22,29 +22,34 @@ class LatestPostNode(Node):
 
 @register.tag(name="get_latest_posts")
 def do_latest_posts(parser, token):
+    asvar = None
+    tag_name, *tokens = token.split_contents()
+    if len(tokens) >= 2 and tokens[-2] == 'as':
+        *tokens, _, asvar = tokens
+    if len(tokens) != 1:
+        raise TemplateSyntaxError(
+            "'{name}' takes 1 argument:"
+            "the number of posts.".format(
+                name=tag_name))
     try:
-        tag_name, number_of_posts_str = (
-            token.split_contents())
+        number_of_posts = int(tokens.pop())
     except ValueError:
         raise TemplateSyntaxError(
-            "get_latest_posts takes 1 argument: "
-            "number of posts to get")
-    try:
-        number_of_posts = int(number_of_posts_str)
-    except ValueError:
-        raise TemplateSyntaxError(
-            "tag '{tag_display}' sole argument "
-            "must be an integer".format(
-                tag_display=tag_name))
-    return LatestPostsNode(number_of_posts)
+            "'{name}' expects argument to be "
+            "an integer.".format(name=tag_name))
+    return LatestPostsNode(number_of_posts, asvar)
 
 
 class LatestPostsNode(Node):
 
-    def __init__(self, number_of_posts):
+    def __init__(self, number_of_posts, asvar):
         self.num = number_of_posts
+        self.asvar = asvar
 
     def render(self, context):
-        context['latest_posts_list'] = (
-            Post.objects.published()[:self.num])
+        objs = Post.objects.published()[:self.num]
+        if self.asvar is None:
+            context['latest_posts_list'] = objs
+        else:
+            context[self.asvar] = objs
         return str()
