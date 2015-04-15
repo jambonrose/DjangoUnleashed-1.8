c21873ba691bdb0d084680259a0634f19db8b723
Andrew Pinkham <code@andrewsforge.com>
2015-04-15 12:38:40 -0400

    Ch03: Define Post model fields. [skip ci]
    Fields are discrete pieces of data within models. They define behavior
    for Django in both Python and our database of choice.
    
        https://docs.djangoproject.com/en/1.8/ref/models/fields/#charfield
        https://docs.djangoproject.com/en/1.8/ref/models/fields/#datefield
        https://docs.djangoproject.com/en/1.8/ref/models/fields/#slugfield
        https://docs.djangoproject.com/en/1.8/ref/models/fields/#textfield

--- blog/models.py
+++ blog/models.py
@@ -6,4 +6,7 @@ from django.db import models
 
 
 class Post(models.Model):
-    pass
+    title = models.CharField(max_length=63)
+    slug = models.SlugField()
+    text = models.TextField()
+    pub_date = models.DateField()
