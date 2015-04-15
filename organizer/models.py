4c76a99e1d72820a367d2195fbd3edc1b0af30fd
Andrew Pinkham <code@andrewsforge.com>
2015-04-15 13:04:48 -0400

    Ch03: Add options to Tag model fields. [skip ci]
    Field options allow us to easily customize behavior of a field.
    
    Global Field Options:
    
        https://docs.djangoproject.com/en/1.8/ref/models/fields/#help-text
        https://docs.djangoproject.com/en/1.8/ref/models/fields/#unique
    
    The max_length field option is defined in CharField and inherited by all
    CharField subclasses (but is typically optional in these subclasses,
    unlike CharField itself).

--- organizer/models.py
+++ organizer/models.py
@@ -6,8 +6,12 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=31)
-    slug = models.SlugField()
+    name = models.CharField(
+        max_length=31, unique=True)
+    slug = models.SlugField(
+        max_length=31,
+        unique=True,
+        help_text='A label for URL config.')
 
 
 class Startup(models.Model):
