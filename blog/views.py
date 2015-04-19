d21e91832bcd2039949a974b96b8279981a93ffb
Andrew Pinkham <code@andrewsforge.com>
2015-04-19 11:58:35 -0500

    Ch05: Remove URL dictionary (revert).

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,7 @@ from django.views.generic import View
 from .models import Post
 
 
-def post_detail(request, year, month,
-                slug, parent_template=None):
+def post_detail(request, year, month, slug):
     post = get_object_or_404(
         Post,
         pub_date__year=year,
@@ -15,15 +14,13 @@ def post_detail(request, year, month,
     return render(
         request,
         'blog/post_detail.html',
-        {'post': post,
-         'parent_template': parent_template})
+        {'post': post})
 
 
 class PostList(View):
 
-    def get(self, request, parent_template=None):
+    def get(self, request):
         return render(
             request,
             'blog/post_list.html',
-            {'post_list': Post.objects.all(),
-             'parent_template': parent_template})
+            {'post_list': Post.objects.all()})
