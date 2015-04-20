d87fc101f2330dfb6452525f97329e617773d16f
Andrew Pinkham <code@andrewsforge.com>
2015-04-20 17:49:53 -0500

    Ch09: Implement ObjectUpdateMixin.

--- organizer/utils.py
+++ organizer/utils.py
@@ -1,4 +1,5 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import (
+    get_object_or_404, redirect, render)
 
 
 class ObjectCreateMixin:
@@ -21,3 +22,37 @@ class ObjectCreateMixin:
                 request,
                 self.template_name,
                 {'form': bound_form})
+
+
+class ObjectUpdateMixin:
+    form_class = None
+    model = None
+    template_name = ''
+
+    def get(self, request, slug):
+        obj = get_object_or_404(
+            self.model, slug__iexact=slug)
+        context = {
+            'form': self.form_class(instance=obj),
+            self.model.__name__.lower(): obj,
+        }
+        return render(
+            request, self.template_name, context)
+
+    def post(self, request, slug):
+        obj = get_object_or_404(
+            self.model, slug__iexact=slug)
+        bound_form = self.form_class(
+            request.POST, instance=obj)
+        if bound_form.is_valid():
+            new_object = bound_form.save()
+            return redirect(new_object)
+        else:
+            context = {
+                'form': bound_form,
+                self.model.__name__.lower(): obj,
+            }
+            return render(
+                request,
+                self.template_name,
+                context)
