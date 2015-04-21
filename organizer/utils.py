8191bf6813379d2ee341df735c84f2956c35b962
Andrew Pinkham <code@andrewsforge.com>
2015-04-21 12:53:53 -0500

    Ch09: Implement ObjectUpdateMixin.

--- organizer/utils.py
+++ organizer/utils.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import (
     get_object_or_404, redirect, render)
 
@@ -22,6 +23,28 @@ class ObjectCreateMixin:
                 request,
                 self.template_name,
                 {'form': bound_form})
+
+
+class ObjectDeleteMixin:
+    model = None
+    success_url = ''
+    template_name = ''
+
+    def get(self, request, slug):
+        obj = get_object_or_404(
+            self.model, slug__iexact=slug)
+        context = {
+            self.model.__name__.lower(): obj,
+        }
+        return render(
+            request, self.template_name, context)
+
+    def post(self, request, slug):
+        obj = get_object_or_404(
+            self.model, slug__iexact=slug)
+        obj.delete()
+        return HttpResponseRedirect(
+            self.success_url)
 
 
 class ObjectUpdateMixin:
