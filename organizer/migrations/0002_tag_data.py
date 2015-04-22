aa0cf8ff695ca2579a2b550d7ebded2f7c90ba58
Andrew Pinkham <code@andrewsforge.com>
2015-04-22 09:49:56 -0500

    Ch10: Code add_tag_data migration function.

--- organizer/migrations/0002_tag_data.py
+++ organizer/migrations/0002_tag_data.py
@@ -3,9 +3,33 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 
+TAGS = (
+    # ( tag name, tag slug ),
+    ("augmented reality", "augmented-reality"),
+    ("big data", "big-data"),
+    ("django", "django"),
+    ("education", "education"),
+    ("ipython", "ipython"),
+    ("javascript", "javascript"),
+    ("jupyter", "jupyter"),
+    ("mobile", "mobile"),
+    ("node.js", "node-js"),
+    ("php", "php"),
+    ("python", "python"),
+    ("ruby on rails", "ruby-on-rails"),
+    ("ruby", "ruby"),
+    ("video games", "video-games"),
+    ("web", "web"),
+    ("zend", "zend"),
+)
+
 
 def add_tag_data(apps, schema_editor):
-    pass
+    Tag = apps.get_model('organizer', 'Tag')
+    for tag_name, tag_slug in TAGS:
+        Tag.objects.create(
+            name=tag_name,
+            slug=tag_slug)
 
 
 def remove_tag_data(apps, schema_editor):
