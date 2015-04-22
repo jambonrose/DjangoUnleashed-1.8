fed5331c72e1b69b835afe7f9ce66819737a8da2
Andrew Pinkham <code@andrewsforge.com>
2015-04-21 23:07:54 -0500

    Ch10: Generate empty migration.
    To start, we need an empty migration file. We can generate one using the
    following command (output included):
    
        $ ./manage.py makemigrations --empty organizer
        Migrations for 'organizer':
          0002_auto_20150515_2300.py:
    
    The file name is simply the date and time (GMT), which is not
    particularly informative. We can rename it.
    
        $ cd organizer/migrations/
        $ mv 0002_auto_20150515_2300.py 0002_tag_data.py

---
+++ organizer/migrations/0002_tag_data.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('organizer', '0001_initial'),
+    ]
+
+    operations = [
+    ]
