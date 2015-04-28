afa6687c317191b77949ba246f3dcc0909c435f5
Andrew Pinkham <code@andrewsforge.com>
2015-04-28 11:35:31 -0500

    Ch17: Revert to Tag Detail URL pattern.

--- organizer/urls/tag.py
+++ organizer/urls/tag.py
@@ -1,10 +1,8 @@
 from django.conf.urls import url
 
-from ..models import Tag
-from ..utils import DetailView
 from ..views import (
-    TagCreate, TagDelete, TagList, TagPageList,
-    TagUpdate)
+    TagCreate, TagDelete, TagDetail, TagList,
+    TagPageList, TagUpdate)
 
 urlpatterns = [
     url(r'^$',
@@ -17,11 +15,7 @@ urlpatterns = [
         TagPageList.as_view(),
         name='organizer_tag_page'),
     url(r'^(?P<slug>[\w\-]+)/$',
-        DetailView.as_view(
-            context_object_name='tag',
-            model=Tag,
-            template_name=(
-                'organizer/tag_detail.html')),
+        TagDetail.as_view(),
         name='organizer_tag_detail'),
     url(r'^(?P<slug>[\w-]+)/delete/$',
         TagDelete.as_view(),
