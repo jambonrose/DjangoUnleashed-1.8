7a1a3eda2163f46ff8892d86b118fb334cd9fbbd
Andrew Pinkham <code@andrewsforge.com>
2015-04-29 19:13:19 -0500

    Ch18: Make PostGetMixin more robust.

--- blog/utils.py
+++ blog/utils.py
@@ -5,10 +5,22 @@ from .models import Post
 
 class PostGetMixin:
 
+    errors = {
+        'url_kwargs':
+            "Generic view {} must be called with "
+            "year, month, and slug.",
+    }
+
     def get_object(self, queryset=None):
         year = self.kwargs.get('year')
         month = self.kwargs.get('month')
         slug = self.kwargs.get('slug')
+        if (year is None
+                or month is None
+                or slug is None):
+            raise AttributeError(
+                self.errors['url_kwargs'].format(
+                    self.__class__.__name__))
         return get_object_or_404(
             Post,
             pub_date__year=year,
