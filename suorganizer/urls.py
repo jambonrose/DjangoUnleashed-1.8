8a685b131db76862648be49c622c979e54ff79fb
Andrew Pinkham <code@andrewsforge.com>
2015-04-30 13:38:36 -0500

    Ch19: Prepare user app for URL Config.

--- suorganizer/urls.py
+++ suorganizer/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth import urls as auth_urls
 from django.views.generic import (
     RedirectView, TemplateView)
 
@@ -23,6 +22,7 @@ from blog import urls as blog_urls
 from contact import urls as contact_urls
 from organizer.urls import (
     startup as startup_urls, tag as tag_urls)
+from user import urls as user_urls
 
 urlpatterns = [
     url(r'^$',
@@ -38,5 +38,5 @@ urlpatterns = [
     url(r'^contact/', include(contact_urls)),
     url(r'^startup/', include(startup_urls)),
     url(r'^tag/', include(tag_urls)),
-    url(r'^user/', include(auth_urls)),
+    url(r'^user/', include(user_urls)),
 ]
