effd75318499c120584e5983c7a5d0929567ddc0
Andrew Pinkham <code@andrewsforge.com>
2015-05-05 11:51:30 -0500

    Ch19: Configure logging settings.

--- suorganizer/settings.py
+++ suorganizer/settings.py
@@ -89,6 +89,38 @@ DATABASES = {
     }
 }
 
+
+# Logging
+# https://docs.djangoproject.com/en/1.8/topics/logging/
+
+verbose = (
+    "[%(asctime)s] %(levelname)s "
+    "[%(name)s:%(lineno)s] %(message)s")
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'formatters': {
+        'verbose': {
+            'format': verbose,
+            'datefmt': "%Y-%b-%d %H:%M:%S"
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'formatter': 'verbose'
+        },
+    },
+}
+
+
 # Email
 # https://docs.djangoproject.com/en/1.8/topics/email/
 
