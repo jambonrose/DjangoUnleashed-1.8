813eca7544fbf064445d6eac55bd2ee83671bdaa
Andrew Pinkham <code@andrewsforge.com>
2015-05-06 10:36:08 -0500

    Ch20: Simplify custom login_required decorator.

--- user/decorators.py
+++ user/decorators.py
@@ -1,24 +1,8 @@
-from functools import wraps
-
-from django.conf import settings
-from django.contrib.auth import get_user
-from django.shortcuts import redirect
-from django.utils.decorators import \
-    available_attrs
+from django.contrib.auth.decorators import \
+    login_required
 
 
 def custom_login_required(view):
     # view argument must be a function
-
-    @wraps(view, assigned=available_attrs(view))
-    def new_view(request, *args, **kwargs):
-        user = get_user(request)
-        if user.is_authenticated():
-            return view(request, *args, **kwargs)
-        else:
-            url = '{}?next={}'.format(
-                settings.LOGIN_URL,
-                request.path)
-            return redirect(url)
-
-    return new_view
+    decorated_view = login_required(view)
+    return decorated_view
