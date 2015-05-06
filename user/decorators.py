546a4681aa54ba183e956d220e98ef67ae6de691
Andrew Pinkham <code@andrewsforge.com>
2015-05-06 10:30:11 -0500

    Ch20: Use functools.wraps to copy view signature.

--- user/decorators.py
+++ user/decorators.py
@@ -1,11 +1,16 @@
+from functools import wraps
+
 from django.conf import settings
 from django.contrib.auth import get_user
 from django.shortcuts import redirect
+from django.utils.decorators import \
+    available_attrs
 
 
 def custom_login_required(view):
     # view argument must be a function
 
+    @wraps(view, assigned=available_attrs(view))
     def new_view(request, *args, **kwargs):
         user = get_user(request)
         if user.is_authenticated():
