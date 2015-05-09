14e2d2282b7c95a1bb6d475faa6d827d90609e16
Andrew Pinkham <code@andrewsforge.com>
2015-05-08 21:51:04 -0500

    Ch23: Define PostAdmin list_display.

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date')
