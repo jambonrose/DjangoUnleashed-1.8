478395e75c877b7ac116575f69391c4834c3100e
Andrew Pinkham <code@andrewsforge.com>
2015-05-08 21:56:48 -0500

    Ch23: Custom fieldsets for PostAdmin.

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,18 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    # list view
     date_hierarchy = 'pub_date'
     list_display = ('title', 'pub_date')
     list_filter = ('pub_date',)
     search_fields = ('title', 'text')
+    # form view
+    fieldsets = (
+        (None, {
+            'fields': (
+                'title', 'slug', 'author', 'text',
+            )}),
+        ('Related', {
+            'fields': (
+                'tags', 'startups')}),
+    )
