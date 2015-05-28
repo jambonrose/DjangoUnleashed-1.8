eb0dc1e1d1ac7460186e1fb8852d0a92c88cc38b
Andrew Pinkham <code@andrewsforge.com>
2015-05-28 09:38:15 -0500

    Ch26: Add debug_toolbar to INSTALLED_APPS.

--- suorganizer/settings.py
+++ suorganizer/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.sites',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'django_extensions',
     'core',
     'organizer',
