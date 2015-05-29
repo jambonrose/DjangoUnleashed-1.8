ebc040b0d714a6c5f85d2e18ff8a04d6edacf66e
Andrew Pinkham <code@andrewsforge.com>
2015-05-29 09:45:05 -0500

    Ch26: Explicitly set template loader settings.

--- suorganizer/settings.py
+++ suorganizer/settings.py
@@ -67,13 +67,16 @@ TEMPLATES = [{
     'DIRS': [
         os.path.join(BASE_DIR, 'templates'),
     ],
-    'APP_DIRS': True,
     'OPTIONS': {
         'context_processors': [
             'django.template.context_processors.debug',
             'django.template.context_processors.request',
             'django.contrib.auth.context_processors.auth',
             'django.contrib.messages.context_processors.messages',
+        ],
+        'loaders': [
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
         ],
     },
 }]
