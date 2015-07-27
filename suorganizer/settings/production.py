0f5887f2f21b6bad24fbed271e3a2fcb6ba1cb79
Andrew Pinkham <code@andrewsforge.com>
2015-07-26 19:31:09 -0500

    Ch29: Configure Postmark settings.

--- suorganizer/settings/production.py
+++ suorganizer/settings/production.py
@@ -30,3 +30,9 @@ DATABASES = {
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+EMAIL_HOST = os.environ.get('POSTMARK_SMTP_SERVER')
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('POSTMARK_API_TOKEN')
+EMAIL_HOST_PASSWORD = os.environ.get('POSTMARK_API_TOKEN')
+EMAIL_USE_TLS = True
