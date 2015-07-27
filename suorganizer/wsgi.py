6e01872f46da06b1f218701ad9796f67c9a8a896
Andrew Pinkham <code@andrewsforge.com>
2015-07-26 20:03:03 -0500

    Ch29: Workaround for Memcache bug.

--- suorganizer/wsgi.py
+++ suorganizer/wsgi.py
@@ -9,10 +9,14 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
+from django.core.cache.backends.memcached import \
+    BaseMemcachedCache
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "suorganizer.settings")
+
+BaseMemcachedCache.close = lambda self, **kwargs: None
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
